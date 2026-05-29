84ebc40de052cd64af81bf1df1daf642a77f3c54
Kevin <thevoidkc@gmail.com>
2026-05-29 13:55:36 -0700

    feat: finish class based components

--- class-component-example/src/components/ClassInput.jsx
+++ class-component-example/src/components/ClassInput.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
+import Count from './Count';
 
 class ClassInput extends Component {
   constructor(props) {
@@ -8,10 +9,16 @@ class ClassInput extends Component {
     this.state = {
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
+      editing: '',
+      editInputVal: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleEditInputChange = this.handleEditInputChange.bind(this);
   }
 
   handleInputChange(e) {
@@ -26,6 +33,35 @@ class ClassInput extends Component {
     this.setState((state) => ({
       todos: state.todos.concat(state.inputVal),
       inputVal: '',
+    }));
+  }
+
+  handleDelete(todoToDelete) {
+    this.setState((state) => ({
+      todos: state.todos.filter((todo) => todo !== todoToDelete),
+    }));
+  }
+
+  handleEdit(todo) {
+    this.setState({
+      editing: todo,
+      editInputVal: todo,
+    });
+  }
+
+  handleConfirm(oldTodo) {
+    this.setState((state) => ({
+      todos: state.todos.map((todo) =>
+        todo === oldTodo ? state.editInputVal : todo,
+      ),
+      editing: '',
+      editInputVal: '',
+    }));
+  }
+
+  handleEditInputChange(e) {
+    this.setState((state) => ({
+      editInputVal: e.target.value,
     }));
   }
 
@@ -46,12 +82,38 @@ class ClassInput extends Component {
           />
           <button type="submit">Submit</button>
         </form>
-        <h4>All the tasks!</h4>
+        <Count count={this.state.todos.length} />
         {/* The list of all the To-Do's, displayed */}
         <ul>
-          {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
-          ))}
+          {this.state.todos.map((todo) =>
+            this.state.editing === todo ? (
+              <li key={todo}>
+                <input
+                  type="text"
+                  value={this.state.editInputVal}
+                  onChange={this.handleEditInputChange}
+                />
+
+                <button type="button" onClick={() => this.handleDelete(todo)}>
+                  X
+                </button>
+
+                <button type="button" onClick={() => this.handleConfirm(todo)}>
+                  Confirm
+                </button>
+              </li>
+            ) : (
+              <li key={todo}>
+                {todo}
+                <button type="button" onClick={() => this.handleDelete(todo)}>
+                  X
+                </button>
+                <button type="button" onClick={() => this.handleEdit(todo)}>
+                  Edit
+                </button>
+              </li>
+            ),
+          )}
         </ul>
       </section>
     );
